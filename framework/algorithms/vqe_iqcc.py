8cb99a1f0cd8af326de9cd55cf1fd117318e6242
Jasper <jasperandrews@me.com>
2026-02-07 14:47:19 -0800

    Added iQCC class and build_ansatz

--- framework/algorithms/vqe_iqcc.py
+++ framework/algorithms/vqe_iqcc.py
@@ -15,3 +15,44 @@ from core.base_vqe import BaseVQE, VQEResult
 from core.hamiltonian_loader import QubitHamiltonian
 
 logger = logging.getLogger(__name__)
+
+class iQCC_VQE(BaseVQE):
+    def __init__(self, 
+                 hamiltonian: QubitHamiltonian, 
+                 max_operators: int=20, 
+                 gradient_threshold: float= 1e-4,
+                 **kwargs):
+        super().__init__(hamiltonian, kwargs)
+        self.name = 'iqcc_vqe'
+        self.description = 'Placeholder description'
+        # define pauli operator pool
+        self.max_operators = max_operators
+        self.gradient_threshold = gradient_threshold
+
+        self.selected_operators = []
+        self.parameters = []
+
+        self.device = None
+        self.cost_fn = None
+
+    def build_ansatz(self):
+        import pennylane as qml
+        from core.backend_manager import create_device
+
+        n_qubits = self.n_qubits
+        device = create_device(self.backend_config)
+
+        def circuit(params, observable):
+            for i in range(n_qubits // 2):
+                qml.PauliX(wires=i)
+            
+            for theta, op in zip(params, self.selected_operators)
+                qml.PauliRot(2 * theta * op.coefficient, op.pauli_word, wires=list(range(n_qubits)))
+            
+            return qml.expval(observable)
+        
+        # TODO: add logger + cost
+        return circuit
+    
+
+        
